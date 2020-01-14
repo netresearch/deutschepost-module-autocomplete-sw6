7154ce80a470fe7cc7a7409c8071bc3f5c57a1ea
Andreas Müller <andreas.mueller@netresearch.de>
2020-01-14 13:26:32 +0000

    PDSW-16: extend address-form template

--- src/Resources/app/storefront/src/dpautocomplete/dpautocomplete.plugin.js
+++ src/Resources/app/storefront/src/dpautocomplete/dpautocomplete.plugin.js
@@ -5,8 +5,19 @@
 import Plugin from 'src/plugin-system/plugin.class';
 
 export default class DPAutocompletePlugin extends Plugin {
+    static options = {
+        token: null,
+        fields: [],
+    };
+
     init()
     {
-        console.log('DP plugin loaded');
+        this.options.fields.forEach(function (field) {
+            console.log(field);
+            const input = document.querySelector(field.selector);
+            input.addEventListener('change', function () {
+                console.log(this.options.fields);
+            }.bind(this));
+        }.bind(this));
     }
 }
