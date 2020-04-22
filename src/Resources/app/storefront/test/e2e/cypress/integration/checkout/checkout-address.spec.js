5cb4911d44ce005e73f096c720c802e76f4b7444
Paul Siedler <paul.siedler@netresearch.de>
2020-04-22 16:26:13 +0200

    PDTWO-40: Update storefront code to use Autocomplete JS library

--- src/Resources/app/storefront/test/e2e/cypress/integration/checkout/checkout-address.spec.js
+++ src/Resources/app/storefront/test/e2e/cypress/integration/checkout/checkout-address.spec.js
@@ -2,28 +2,30 @@
 describe('PostdirektAutocompleteCypressTest: Test checkout address', () => {
     it('go to checkout and fill billing address', () => {
 
-        let product = cy.getRandomProductInformationForCheckout();
-        cy.visit(product.url);
-        cy.get('.btn-buy').first().click();
+        let product = cy.getRandomProductInformationForCheckout().then(product => {
+            cy.visit(product.url);
+            cy.get('.btn-buy').first().click();
 
-        // Off canvas
-        cy.get('.offcanvas .is-open').should('be.visible');
+            // Off canvas
+            cy.get('.offcanvas.is-open').should('be.visible');
 
-        // Checkout
-        cy.get('.offcanvas-cart-actions .btn-primary').click();
+            // Checkout
+            cy.get('.offcanvas-cart-actions .btn-primary').click();
 
-        cy.get('.register-card').should('be.visible');
-        cy.get('select[name="billingAddress[countryId]"]').select('Germany');
-        cy.get('input[name="billingAddress[zipcode]"]').type('042');
-        cy.wait(200).then(() => {
-            cy.get('#datalist-billingAddressAddressZipcode').should('be.visible');
+            cy.get('.register-card').should('be.visible');
+            cy.get('select[name="billingAddress[countryId]"]').select('Germany');
+            cy.get('input[name="billingAddress[zipcode]"]').type('042');
+            cy.wait(200).then(() => {
+                cy.get('#datalist-billingAddressAddressZipcode').should('be.visible');
+            });
+            cy.get('#datalist-billingAddressAddressZipcode').find('li').first().should('contain', 'Leipzig');
+            cy.get('#datalist-billingAddressAddressZipcode').find('li').first().click().then(($li) => {
+                let zip = $li.get(0).innerText.split(',')[0];
+
+                cy.get('input[name="billingAddress[zipcode]"]').should('have.value', zip);
+                cy.get('input[name="billingAddress[city]"]').should('have.value', 'Leipzig');
+            });
         });
-        cy.get('#datalist-billingAddressAddressZipcode').find('li').first().should('contain', 'Leipzig');
-        cy.get('#datalist-billingAddressAddressZipcode').find('li').first().click().then(($li) => {
-            let zip = $li.get(0).innerText.split(',')[0];
 
-            cy.get('input[name="billingAddress[zipcode]"]').should('have.value', zip);
-            cy.get('input[name="billingAddress[city]"]').should('have.value', 'Leipzig');
-        });
     });
 });
