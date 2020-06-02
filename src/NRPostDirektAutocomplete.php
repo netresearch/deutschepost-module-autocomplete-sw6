935d7afaea18be80dc859952de2c771beba60630
Max Melzer <max.melzer@netresearch.de>
2020-06-02 13:53:38 +0000

    PDSW-42: Add deployment tasks and store metadata

--- src/NRPostDirektAutocomplete.php
+++ src/NRPostDirektAutocomplete.php
@@ -10,6 +10,11 @@ namespace PostDirekt\Autocomplete;
 
 use Shopware\Core\Framework\Plugin;
 
+$autoloadPath = __DIR__ . '/../vendor/autoload.php';
+if (!defined('__NR_POSTDIREKT_MANAGED_BY_COMPOSER') && file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+}
+
 class NRPostDirektAutocomplete extends Plugin
 {
 }
