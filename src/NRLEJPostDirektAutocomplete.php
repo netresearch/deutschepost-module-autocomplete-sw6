7a5929f52a601d5b8922e520e14ecbe9c9b24bb9
Paul Siedler <paul.siedler@netresearch.de>
2021-02-23 15:16:46 +0000

    CI-262: change whitelist to avoid scoping common interfaces already available in shopware, switch http-client-implementation due to issues with scoped functions

--- src/NRLEJPostDirektAutocomplete.php
+++ src/NRLEJPostDirektAutocomplete.php
@@ -10,13 +10,13 @@ namespace PostDirekt\Autocomplete;
 
 use Shopware\Core\Framework\Plugin;
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+if (\file_exists(__DIR__ . '/../vendor/autoload.php')) {
     $autoloadPath = __DIR__ . '/../vendor/autoload.php';
 }
-if (file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
+if (\file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
     $autoloadPath = __DIR__ . '/../vendor/scoper-autoload.php';
 }
-if (!defined('__NR_POSTDIREKT_AUTOCOMPLETE_MANAGED_BY_COMPOSER') && isset($autoloadPath)) {
+if (\defined('__NR_POSTDIREKT_AUTOCOMPLETE_MANAGED_BY_COMPOSER') === false && isset($autoloadPath)) {
     require_once $autoloadPath;
 }
 
